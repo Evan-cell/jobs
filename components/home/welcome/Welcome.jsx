a70051f70216f3c0fb1132831d4d1e2182e4ffae
Evan-cell <60639510+Evan-cell@users.noreply.github.com>
2023-03-25 15:24:22 +0300

    jobs

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useRouter } from 'expo-router'
+import { icons, SIZES } from "../../../constants";
+import { View, Text, FlatList, TextInput, TouchableOpacity } from 'react-native'
+
+import styles from './welcome.style'
+
+const Welcome = () => {
+  return (
+    <View>
+      <Text>Welcome</Text>
+    </View>
+  )
+}
+
+export default Welcome
+// git remote add origin https://github.com/Evan-cell/jobs.git
